bb9a61bd35d239c0c060d8ebc0e7d48fd3e10732
Nambi94444 <nambi94444@gmail.com>
2023-04-21 23:52:36 +0530

    upload

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -2,32 +2,6 @@ import Classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItems/MealItem";
 import { useEffect, useState } from "react";
-// const DUMMY_MEALS = [
-//   {
-//     id: "m1",
-//     name: "Sushi",
-//     description: "Finest fish and veggies",
-//     price: 22.99,
-//   },
-//   {
-//     id: "m2",
-//     name: "Schnitzel",
-//     description: "A german specialty!",
-//     price: 16.5,
-//   },
-//   {
-//     id: "m3",
-//     name: "Barbecue Burger",
-//     description: "American, raw, meaty",
-//     price: 12.99,
-//   },
-//   {
-//     id: "m4",
-//     name: "Green Bowl",
-//     description: "Healthy...and green...",
-//     price: 18.99,
-//   },
-// ];
 
 const AvailableMeals = () => {
   const [fooditems, setfooditems] = useState([]);
